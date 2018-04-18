f18c762c6d7a82d5324e0cbeb44edc9035e2e0dc
Anthony Lee <anthonyylee@utexas.edu>
2018-04-17 22:34:22 -0500

    Added paper-onboarding pod - for onboarding screen

---
+++ Moodoo/Storyboards_Anthony/OnboardingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingViewController.swift
+//  Moodoo
+//
+//  Created by Anthony Lee on 4/17/18.
+//  Copyright © 2018 Group6. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+}
