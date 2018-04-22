73c9f99389a7a213560e2b25a4c9bcf604c66de3
Anthony Lee <anthonyylee@utexas.edu>
2018-04-22 18:51:07 -0500

    unzipped integrated [storyboard] folder, this is the most updated one

---
+++ Moodoo/Storyboards_Natascha/ShowMoodView.swift
@@ -0,0 +1,51 @@
+//
+//  ShowMoodView.swift
+//  Moodoo2
+//
+//  Created by Natascha Brauchle on 4/17/18.
+//  Copyright © 2018 Natascha Brauchle. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ShowMoodView: UIViewController {
+    
+    @IBOutlet weak var reasonsLabel: UILabel!
+    @IBOutlet weak var sleepLabel: UILabel!
+    @IBOutlet weak var anxiousLabel: UILabel!
+    @IBOutlet weak var angryLabel: UILabel!
+    @IBOutlet weak var excitedLabel: UILabel!
+    @IBOutlet weak var sadLabel: UILabel!
+    @IBOutlet weak var happyLabel: UILabel!
+    var mood: NSManagedObject? = nil
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.reasonsLabel.text = mood!.value(forKey: "reasons") as? String
+        self.excitedLabel.text = mood!.value(forKey: "excited") as? String
+        self.happyLabel.text = mood!.value(forKey: "happy") as? String
+        self.sadLabel.text = mood!.value(forKey: "sad") as? String
+        self.angryLabel.text = mood!.value(forKey: "angry") as? String
+        self.anxiousLabel.text = mood!.value(forKey: "anxious") as? String
+        self.sleepLabel.text = mood!.value(forKey: "sleep") as? String
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Old/Moodoo/ViewControllers/LoginViewController.swift
@@ -0,0 +1,69 @@
+//
+//  LoginViewController.swift
+//  Moodoo
+//
+//  Created by Michael Moscoso on 3/29/18.
+//  Copyright © 2018 Group6. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var txtUsername: UITextField!    // Username input field
+    @IBOutlet weak var txtPassword: UITextField!    // Password input field
+    
+    private var alertController:UIAlertController?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        PersistenceService.shared.fetchUsers()      // Impliment Firebase as DB later
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnLogin(_ sender: Any) {
+        if txtUsername.text == "" || txtPassword.text == "" {
+            self.alertController = UIAlertController(title: "Invalid Login", message: "Please enter both Username and Password", preferredStyle: UIAlertControllerStyle.alert)
+            let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action:UIAlertAction) in }
+            self.alertController!.addAction(OKAction)
+            self.present(self.alertController!, animated:true, completion:nil)
+        }
+        else {
+            let user = PersistenceService.shared.getUser(name: txtUsername.text!)
+            
+            if user.username == "<bad>" {
+                self.alertController = UIAlertController(title: "Invalid Login", message: "Username not recognized", preferredStyle: UIAlertControllerStyle.alert)
+                let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action:UIAlertAction) in }
+                self.alertController!.addAction(OKAction)
+                self.present(self.alertController!, animated:true, completion:nil)
+            }
+            else {
+                if user.password != txtPassword.text {
+                    self.alertController = UIAlertController(title: "Invalid Login", message: "Password does not match our records", preferredStyle: UIAlertControllerStyle.alert)
+                    let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (action:UIAlertAction) in }
+                    self.alertController!.addAction(OKAction)
+                    self.present(self.alertController!, animated:true, completion:nil)
+                }
+                else {
+                    // THEN YOU CAN FINALLY LOG IN
+                }
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
+    */
+
+}
