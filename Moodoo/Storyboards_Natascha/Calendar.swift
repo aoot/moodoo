900b45280593d8686c0e8d21cd2a62cf9f14efa9
Anthony Lee <anthonyylee@utexas.edu>
2018-04-23 13:48:10 -0500

    Comment out all code because naming convention is conflicting with CalendarViewViewController

--- Moodoo/Storyboards_Natascha/Calendar.swift
+++ Moodoo/Storyboards_Natascha/Calendar.swift
@@ -6,139 +6,139 @@
 //  Copyright © 2018 Natascha Brauchle. All rights reserved.
 //
 
-import UIKit
-import CoreData
-
-class Calendar: UITableViewController {
-
-    var moods = [NSManagedObject]()
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let managedContext = appDelegate.managedObjectContext
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName:"Mood")
-        var fetchedResults:[NSManagedObject]? = nil
-        
-        do {
-            try fetchedResults = managedContext.fetch(fetchRequest) as? [NSManagedObject]
-        } catch {
-            // what to do if an error occurs?
-            let nserror = error as NSError
-            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
-            abort()
-        }
-        
-        if let results = fetchedResults {
-            moods = results
-        } else {
-            print("Could not fetch")
-        }
-        
-        self.tableView.reloadData()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    // MARK: - Table view data source
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return moods.count
-    }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "moodID", for: indexPath)
-        let mood = moods[indexPath.row]
-        
-        cell.textLabel!.text = "\(mood.value(forKey: "date") as! String)"
-       
-        return cell
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let destinationVC = segue.destination as? ShowMoodView,
-            let selectedIndexPath = tableView.indexPathForSelectedRow {
-            destinationVC.mood = moods[selectedIndexPath.row]
-        }
-    }
-    //override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-      //  if let destinationVC = segue.destination as? ShowMoodView,
-        //    let selectedIndexPath = tableView.indexPathForSelectedRow {
-         //   destinationVC.candidate = moods[selectedIndexPath.row]
-        //}
-    //}
-    
-    
-    /*
-     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-     
-     // Configure the cell...
-     
-     return cell
-     }
-     */
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
-}
+//import UIKit
+//import CoreData
+//
+//class Calendar: UITableViewController {
+//
+//    var moods = [NSManagedObject]()
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        // Uncomment the following line to preserve selection between presentations
+//        // self.clearsSelectionOnViewWillAppear = false
+//        
+//        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+//        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+//    }
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        
+//        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+//        let managedContext = appDelegate.managedObjectContext
+//        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName:"Mood")
+//        var fetchedResults:[NSManagedObject]? = nil
+//        
+//        do {
+//            try fetchedResults = managedContext.fetch(fetchRequest) as? [NSManagedObject]
+//        } catch {
+//            // what to do if an error occurs?
+//            let nserror = error as NSError
+//            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+//            abort()
+//        }
+//        
+//        if let results = fetchedResults {
+//            moods = results
+//        } else {
+//            print("Could not fetch")
+//        }
+//        
+//        self.tableView.reloadData()
+//    }
+//    
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//    
+//    // MARK: - Table view data source
+//    
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 1
+//    }
+//    
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return moods.count
+//    }
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "moodID", for: indexPath)
+//        let mood = moods[indexPath.row]
+//        
+//        cell.textLabel!.text = "\(mood.value(forKey: "date") as! String)"
+//       
+//        return cell
+//    }
+//    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if let destinationVC = segue.destination as? ShowMoodView,
+//            let selectedIndexPath = tableView.indexPathForSelectedRow {
+//            destinationVC.mood = moods[selectedIndexPath.row]
+//        }
+//    }
+//    //override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//      //  if let destinationVC = segue.destination as? ShowMoodView,
+//        //    let selectedIndexPath = tableView.indexPathForSelectedRow {
+//         //   destinationVC.candidate = moods[selectedIndexPath.row]
+//        //}
+//    //}
+//    
+//    
+//    /*
+//     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+//     
+//     // Configure the cell...
+//     
+//     return cell
+//     }
+//     */
+//    
+//    /*
+//     // Override to support conditional editing of the table view.
+//     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+//     // Return false if you do not want the specified item to be editable.
+//     return true
+//     }
+//     */
+//    
+//    /*
+//     // Override to support editing the table view.
+//     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+//     if editingStyle == .delete {
+//     // Delete the row from the data source
+//     tableView.deleteRows(at: [indexPath], with: .fade)
+//     } else if editingStyle == .insert {
+//     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+//     }
+//     }
+//     */
+//    
+//    /*
+//     // Override to support rearranging the table view.
+//     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+//     
+//     }
+//     */
+//    
+//    /*
+//     // Override to support conditional rearranging of the table view.
+//     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+//     // Return false if you do not want the item to be re-orderable.
+//     return true
+//     }
+//     */
+//    
+//    /*
+//     // MARK: - Navigation
+//     
+//     // In a storyboard-based application, you will often want to do a little preparation before navigation
+//     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//     // Get the new view controller using segue.destinationViewController.
+//     // Pass the selected object to the new view controller.
+//     }
+//     */
+//    
+//}
