1992187bc705de106bfe0e179a45a98a593c1406
Natascha Brauchle <brauchle@hinq.cs.utexas.edu>
2018-04-30 22:05:44 -0500

    Calendar looks good but we still need to link each cell properly. Tried coloring cell but get error that it is not key compliant but couldn't work it out. I think there's a connection that isn't correct or something, but I'll look into it tomorrow.

--- Moodoo/ViewControllers/CalendarViewViewController.swift
+++ Moodoo/ViewControllers/CalendarViewViewController.swift
@@ -22,8 +22,8 @@ class CalendarViewViewController: UIViewController {
     }
 
     func setupCalendarView(){
-        calendarView.minimumLineSpacing = 0
-        calendarView.minimumInteritemSpacing = 0
+        //calendarView.minimumLineSpacing = 0
+        //calendarView.minimumInteritemSpacing = 0
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -46,73 +46,50 @@ class CalendarViewViewController: UIViewController {
 extension CalendarViewViewController:  JTAppleCalendarViewDataSource {
 
     func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
-        var startDate = "1999 12 10"
-        let fetchRequest = NSFetchRequest<Mood>(entityName: "Mood")
-        do {
-            // fetch date of first entry and set it to the start date of the calendar
-            let appDelegate = UIApplication.shared.delegate as! AppDelegate
-            // Getting managed object context
-            let managedObjectContext = appDelegate.persistentContainer.viewContext
-            let fetchedResults = try managedObjectContext.fetch(fetchRequest)
-            if fetchedResults.count > 0 {
-                let moodObject: Mood = fetchedResults[0]
-                startDate = (moodObject.date!)
-            }
-            else{
-                // if there isn't another entry, start date should be first of the current month
-                let dateFormatter = DateFormatter()
-                let date = NSDate()
-                dateFormatter.dateFormat = "yyyy-MM-dd"
-                let calendar = NSCalendar.current
-                let components = Calendar.current.dateComponents([.year, .month], from: Date())
-                startDate = String(describing: Calendar.current.date(from: components)!)
-            }
-        } catch let error as NSError {
-            // something went wrong, print the error.
-            print(error.description)
-        }
+
         
         formatter.dateFormat = "yyyy MM dd"
         formatter.timeZone = Calendar.current.timeZone
         formatter.locale = Calendar.current.locale
        
-        let finalStartDate = formatter.date(from: startDate)!
-        let endDate = formatter.date(from: "2199 12 31")!
+        let finalStartDate = formatter.date(from: "2018 01 01")!
+        let endDate = formatter.date(from: "2018 12 31")!
         
         let parameters = ConfigurationParameters(startDate: finalStartDate, endDate: endDate)
         return parameters
     }
     
-    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
-       // What the fuck does this do
+   func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+       // makes other extension work somehow
     }
     
 }
 
 extension CalendarViewViewController: JTAppleCalendarViewDelegate {
     func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        // display the cell
         let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
         cell.dateLabel.text = cellState.text
         
-        if cellState.isSelected {
-            cell.selectedView.isHidden = false
-        }
-        else{
-            cell.selectedView.isHidden = true
-        }
+       // if cellState.isSelected {
+         //   cell.selectedView.isHidden = false
+        //}
+        //else{
+          //cell.selectedView.isHidden = true
+        //}
         return cell
     }
     
-    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
-        guard let validCell = cell as? CustomCell else {return}
-        // show visibility of selected view
-        validCell.selectedView.isHidden = false
-    }
+    //func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+       // guard let validCell = cell as? CustomCell else {return}
+         //show visibility of selected view
+       // validCell.selectedView.isHidden = false
+  //  }
     
-    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
-        guard let validCell = cell as? CustomCell else {return}
-        // show visibility of selected view
-        validCell.selectedView.isHidden = true
-    }
+    //func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+      //  guard let validCell = cell as? CustomCell else {return}
+         //show visibility of selected view
+        //validCell.selectedView.isHidden = true
+    //}
 
 }
